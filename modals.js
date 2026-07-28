d8792ce81685425829d8ff6812bb15e21e5e7d9f
brendandesigns <61992175+brendandesigns@users.noreply.github.com>
2026-07-28 20:44:19 +0100

    Add files via upload

---
+++ modals.js
@@ -0,0 +1,203 @@
+const openModalOne = document.getElementById('modalOne');
+const openModalTwo = document.getElementById('modalTwo');
+const openModalThree = document.getElementById('modalThree');
+const openModalFour = document.getElementById('modalFour');
+const openModalFive = document.getElementById('modalFive');
+const openModalSix = document.getElementById('modalSix');
+const openModalSeven = document.getElementById('modalSeven');
+const openModalEight = document.getElementById('modalEight');
+const openModalNine = document.getElementById('modalNine');
+
+const openModalEleven = document.getElementById('modalEleven');
+
+const openModalThirteen = document.getElementById('modalThirteen');
+
+const openModalFifteen = document.getElementById('modalFifteen');
+
+/* NEW PROJECTS */
+const openModalSixteen = document.getElementById('modalSixteen');
+const openModalSeventeen = document.getElementById('modalSeventeen');
+const openModalEighteen = document.getElementById('modalEighteen');
+
+
+
+
+const modalContainerOne = document.getElementById('contentOne');
+const modalContainerTwo = document.getElementById('contentTwo');
+const modalContainerThree = document.getElementById('contentThree');
+const modalContainerFour = document.getElementById('contentFour');
+const modalContainerFive = document.getElementById('contentFive');
+const modalContainerSix = document.getElementById('contentSix');
+const modalContainerSeven = document.getElementById('contentSeven');
+const modalContainerEight = document.getElementById('contentEight');
+const modalContainerNine = document.getElementById('contentNine');
+const modalContainerEleven = document.getElementById('contentEleven');
+const modalContainerThirteen = document.getElementById('contentThirteen');
+const modalContainerFifteen = document.getElementById('contentFifteen');
+const modalContainerSixteen = document.getElementById('contentSixteen');
+const modalContainerSeventeen = document.getElementById('contentSeventeen');
+const modalContainerEighteen = document.getElementById('contentEighteen');
+
+
+const closeModalOne = document.getElementById('closeModalOne');
+const closeModalTwo = document.getElementById('closeModalTwo');
+const closeModalThree = document.getElementById('closeModalThree');
+const closeModalFour = document.getElementById('closeModalFour');
+const closeModalFive = document.getElementById('closeModalFive');
+const closeModalSix = document.getElementById('closeModalSix');
+const closeModalSeven = document.getElementById('closeModalSeven');
+const closeModalEight = document.getElementById('closeModalEight');
+const closeModalNine = document.getElementById('closeModalNine');
+const closeModalEleven = document.getElementById('closeModalEleven');
+const closeModalThirteen = document.getElementById('closeModalThirteen');
+const closeModalFifteen = document.getElementById('closeModalFifteen');
+const closeModalSixteen = document.getElementById('closeModalSixteen');
+const closeModalSeventeen = document.getElementById('closeModalSeventeen');
+const closeModalEighteen = document.getElementById('closeModalEighteen');
+
+
+
+
+openModalOne.addEventListener('click', () => {
+    modalContainerOne.classList.add('show');
+});
+
+openModalTwo.addEventListener('click', () => {
+    modalContainerTwo.classList.add('show');
+});
+
+openModalThree.addEventListener('click', () => {
+    modalContainerThree.classList.add('show');
+});
+
+openModalFour.addEventListener('click', () => {
+    modalContainerFour.classList.add('show');
+});
+
+openModalFive.addEventListener('click', () => {
+    modalContainerFive.classList.add('show');
+});
+
+openModalSix.addEventListener('click', () => {
+    modalContainerSix.classList.add('show');
+});
+
+openModalSeven.addEventListener('click', () => {
+    modalContainerSeven.classList.add('show');
+});
+
+openModalEight.addEventListener('click', () => {
+    modalContainerEight.classList.add('show');
+});
+
+openModalNine.addEventListener('click', () => {
+    modalContainerNine.classList.add('show');
+});
+
+openModalEleven.addEventListener('click', () => {
+    modalContainerEleven.classList.add('show');
+});
+
+openModalThirteen.addEventListener('click', () => {
+    modalContainerThirteen.classList.add('show');
+});
+
+openModalFifteen.addEventListener('click', () => {
+    modalContainerFifteen.classList.add('show');
+});
+
+openModalSixteen.addEventListener('click', () => {
+    modalContainerSixteen.classList.add('show');
+});
+
+openModalSeventeen.addEventListener('click', () => {
+    modalContainerSeventeen.classList.add('show');
+});
+
+openModalEighteen.addEventListener('click', () => {
+    modalContainerEighteen.classList.add('show');
+});
+
+
+
+
+
+closeModalOne.addEventListener('click', () => {
+    modalContainerOne.classList.remove('show');
+});
+
+closeModalTwo.addEventListener('click', () => {
+    modalContainerTwo.classList.remove('show');
+});
+
+closeModalThree.addEventListener('click', () => {
+    modalContainerThree.classList.remove('show');
+});
+
+closeModalFour.addEventListener('click', () => {
+    modalContainerFour.classList.remove('show');
+});
+
+closeModalFive.addEventListener('click', () => {
+    modalContainerFive.classList.remove('show');
+});
+
+closeModalSix.addEventListener('click', () => {
+    modalContainerSix.classList.remove('show');
+});
+
+closeModalSeven.addEventListener('click', () => {
+    modalContainerSeven.classList.remove('show');
+});
+
+closeModalEight.addEventListener('click', () => {
+    modalContainerEight.classList.remove('show');
+});
+
+closeModalNine.addEventListener('click', () => {
+    modalContainerNine.classList.remove('show');
+});
+
+
+closeModalEleven.addEventListener('click', () => {
+    modalContainerEleven.classList.remove('show');
+});
+
+
+
+closeModalThirteen.addEventListener('click', () => {
+    modalContainerThirteen.classList.remove('show');
+});
+
+
+closeModalFifteen.addEventListener('click', () => {
+    modalContainerFifteen.classList.remove('show');
+});
+
+closeModalSixteen.addEventListener('click', () => {
+    modalContainerSixteen.classList.remove('show');
+});
+
+closeModalSeventeen.addEventListener('click', () => {
+    modalContainerSeventeen.classList.remove('show');
+});
+
+closeModalEighteen.addEventListener('click', () => {
+    modalContainerEighteen.classList.remove('show');
+});
+
+
+/* 
+
+function outsideClick(e) {
+    if (e.target === modalContainerOne) 
+        modalContainerOne.style.display = 'none'; 
+
+    if (e.target === modalContainerTwo) 
+        modalContainerTwo.style.display = 'none'; 
+  };
+
+
+  window.addEventListener('click', outsideClick);
+
+*/
